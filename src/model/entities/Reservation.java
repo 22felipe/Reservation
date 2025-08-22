43474303bacf7a9ea8029ae0d0126e0e77e449d6
Felipe Gabriel <felipegmarques32fg@gmail.com>
2025-08-21 22:12:22 -0300

    final

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,74 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) throws DomainException {
+
+        if (!checkout.after(checkin)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+
+        Date now = new Date();
+
+        if (checkin.before(now) || checkout.before(now)) {
+            throw new DomainException("Reservation dates must be future dates");
+        }
+
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Integer duration() {
+        long diffInMillis = checkout.getTime() - checkin.getTime();
+        return (int) TimeUnit.DAYS.convert(diffInMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkin, Date checkout) throws DomainException {
+
+        Date now = new Date();
+
+        if (checkin.before(now) || checkout.before(now)) {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+
+        if (!checkout.after(checkin)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "Room " + this.roomNumber
+                + ", check-in: " + sdf.format(checkin)
+                + ", check-out: " + sdf.format(checkout)
+                + ", " + duration() + " nights";
+    }
+}
